ac24b47245d86b462795d3a45a2951e8c50f06f1
mvogelgesang <mvogelgesang@salesforce.com>
2024-02-12 21:35:44 -0500

    Added orderId key to order-items and ensured both are strings

--- src/schemas/order-item.ts
+++ src/schemas/order-item.ts
@@ -1,6 +1,6 @@
-import { Product } from "./product";
 export interface OrderItem {
   id: string;
+  orderId: string;
   productId: string;
   quantity: number;
   price: number;
--- src/schemas/order.ts
+++ src/schemas/order.ts
@@ -1,5 +1,5 @@
 export interface Order {
-  id: number;
+  id: string;
   customerId: string;
   totalPrice: number;
   createdAt: Date;
