6049cfe2dc127ac454943aa7b8ad5ea1272d1eac
mvogelgesang <mvogelgesang@salesforce.com>
2024-02-15 22:03:33 -0500

    Added colors, sizes, and department to product. Colors and sizes are produced as arrays which can be transformed in data cloud.

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -1,6 +1,9 @@
 export interface Product {
   id: string;
   name: string;
+  colors: string[];
+  sizes: string[];
+  department: string;
   description: string;
   price: number;
   createdAt: Date;
